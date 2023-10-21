f67332b76ad1854a9bb2e85f5560c126436039ae
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-10-21 13:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
-  let initial=Infinite, ans=-1;
+  let initial=Infinite
+	  let ans=-1;
 	let n=arr.length;
 	for(let i=0;i<n-3;i++){
 		for(let j=i+1;j<n-2;j++){
 			for(let k=j+1;k<n-1;k++){
-				sum=arr[i]+arr[j]+arr[k];
-				if(initial > sum){
+				sum=arr[i]+arr[j]+arr[k]
+				let diff= Math.abs(sum-target)
+				if(initial > diff){
 					ans=sum;
 				}
 			}
